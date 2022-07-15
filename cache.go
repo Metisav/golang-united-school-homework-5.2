aae1f11cb44beceb4bf6e98c3abe595f8f3d5b2b
artem <metisav@gmail.com>
2022-07-15 18:29:09 +0500

    solution

--- cache.go
+++ cache.go
@@ -2,22 +2,53 @@ package cache
 
 import "time"
 
+type Data struct {
+	value    string
+	deadline *time.Time
+}
+
 type Cache struct {
+	Map map[string]Data
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		Map: map[string]Data{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func NewData(value string, deadline *time.Time) *Data {
+	return &Data{value, deadline}
 }
 
-func (receiver) Put(key, value string) {
+func (cache Cache) Get(key string) (string, bool) {
+	cache.cleanup()
+	value, err := cache.Map[key]
+	return value.value, err
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Put(key, value string) {
+	cache.Map[key] = *NewData(value, nil)
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache Cache) Keys() []string {
+	cache.cleanup()
+	keys := make([]string, 0)
+	for k := range cache.Map {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.Map[key] = *NewData(value, &deadline)
+}
+
+func (cache *Cache) cleanup() {
+	current_time := time.Now()
+	for k, v := range cache.Map {
+		if v.deadline.Before(current_time) {
+			delete(cache.Map, k)
+		}
+	}
 }
